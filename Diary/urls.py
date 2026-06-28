fad97979029a6c7520cc0fe53c3bcc6d53069964
Abhinavpardhu <pardhunagunuri@gmail.com>
2026-06-28 17:14:48 +0530

    login html page

--- Diary/urls.py
+++ Diary/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Home.views import home
-
+from authentication.views import login
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home, name='home')
+    path('',home, name='home'),
+    path('login/', login, name='login'),
 ]
